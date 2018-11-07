68b77e813a769df2ce62e7c03d78a9648d8647d3
Seong <kirakim96@hotmail.com>
2018-11-07 17:07:49 -0600

    editing

--- src/index.js
+++ src/index.js
@@ -4,18 +4,29 @@ import {chooseRandom, createPrompt, createQuestions} from './lib'
 // TODO read in the file createPrompt.json, run the object read in through createPrompt,
 // pass the result of createPrompt to chooseRandom, and then save the result to
 // cp_solutions.json
-/*
-createPrompt({ numQuestions: 2, numChoices: 2 })
-
-createQuestions({
-    'question-1': 'Do you think you\'re ready for this?',
-    'question-1-choice-1': 'Beyond ready!!!',
-    'question-1-choice-2': 'Totally!',
-    'question-2': 'Are you loving JS yet?',
-    'question-2-choice-1': 'It\'s tubular!',
-    'question-2-choice-2': 'Way rad man!'
-  })*/
+console.log(readFromFile('/lib/createPrompt.json'))
+console.log(writeToFile('/lib/cp_solutions.json'))
 
 
 // TODO read the file createQuestions.json. Pass the read in object to createQuestions,
 // pass its result to chooseRandom, and then save the final result to cq_solutions.json
+console.log(readFromFile('/lib/createQuestions.json'))
+console.log(writeToFile('/lib/cq_solutions.json'))
+
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status==200) {
+      result = xmlhttp.responseText;
+    }
+    return result;
+}
+
+function readFromFile(filePath) {
+    return filePath
+}
+function writeToFile(filePath) {
+    return filePath
+}
