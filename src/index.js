c1971cba8d807cf91f1dcdbed6a134efa952642a
Seong <kirakim96@hotmail.com>
2018-11-09 10:32:08 -0600

    Read Write file

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
-// TODO import readFile, writeFile, chooseRandom, createPrompt, and createQuestions
+// Import readFile, writeFile, chooseRandom, createPrompt, and createQuestions
 import {chooseRandom, createPrompt, createQuestions} from './lib'
+//Import read and write files
+import {readFile,writeFile} from 'fs'
 
-// TODO read in the file createPrompt.json, run the object read in through createPrompt,
+//Read file, do a function on it, choosRandom, and then output
+const readWrite = (f,fileOutput) => (err, data) => {
+    if(err) {//throw err
+        console.log(err)
+    }
+    else {
+        writeFile(fileOutput, JSON.stringify(
+            chooseRandom(f(JSON.parse(data))),null,2),
+                (err) => {if(err) {console.log(err)} else console.log('File saved successfully')})
+    }
+}
+
+// Read in the file createPrompt.json, run the object read in through createPrompt,
 // pass the result of createPrompt to chooseRandom, and then save the result to
 // cp_solutions.json
+const readWritePrompt = readWrite(createPrompt,'cp_solutions.json')
+readFile('lib/createPrompt.json',readWritePrompt)
 
-//console.log(readFromFile('/lib/createPrompt.json'))
-//console.log(writeToFile('/lib/cp_solutions.json'))
-//console.log(createPrompt({numQuestions: 2, numChoices: 2}))
-//console.log(createPrompt())
-
-// TODO read the file createQuestions.json. Pass the read in object to createQuestions,
+// Read the file createQuestions.json. Pass the read in object to createQuestions,
 // pass its result to chooseRandom, and then save the final result to cq_solutions.json
-
-//console.log(readFromFile('/lib/createQuestions.json'))
-//console.log(writeToFile('/lib/cq_solutions.json'))
-
-console.log(createQuestions({
-    'question-1': 'Do you think you\'re ready for this?',
-    'question-1-choice-1': 'Beyond ready!!!',
-    'question-1-choice-2': 'Totally!',
-    'question-2': 'Are you loving JS yet?',
-    'question-2-choice-1': 'It\'s tubular!',
-    'question-2-choice-2': 'Way rad man!'
-  }))
-
-
-
-function readFromFile(filePath) {
-    return filePath
-}
-function writeToFile(filePath) {
-    return filePath
-}
+const readWriteQuestions = readWrite(createQuestions,'cq_solutions.json')
+readFile('lib/createQuestions.json',readWriteQuestions)
